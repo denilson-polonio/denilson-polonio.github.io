4d668ecc6d419f5a0b8053726e4a5f622cd595d6
Denilson <poloniodenilson@gmail.com>
2024-05-21 12:36:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const toggleSwitch = document.querySelector(
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("data-theme", "light");
   } else {
     document.documentElement.setAttribute("data-theme", "dark");
   }
@@ -37,10 +37,10 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-  } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark"); //add this
   }
 }
@@ -54,7 +54,7 @@ const currentTheme = localStorage.getItem("theme")
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === "dark") {
+  if (currentTheme === "light") {
     toggleSwitch.checked = true;
   }
 }
